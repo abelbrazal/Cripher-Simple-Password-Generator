2739f249594869dc6db0e6482b6c7b17032c062e
Abel Brazal <abelbrazal@gmail.com>
2026-05-20 08:58:56 +0800

    Initial commit on improvements_v1

---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+class  PasswordGenerator
+{
+    static void Main()
+    {
+        Console.WriteLine("Random Password generator");
+
+        //This is where you put your prefered password lenght.
+        Console.Write("Enter password length: ");
+        int length = int.Parse(Console.ReadLine());
+
+    }
+}
